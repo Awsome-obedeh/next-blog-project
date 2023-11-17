66c765799f9e4efe098b72e7f5c986d5c0f6075f
obed chidera <obedchidera1010@gmail.com>
2023-11-17 16:57:44 +0100

    categories page

---
+++ src/app/portfolio/[category]/page.jsx
@@ -0,0 +1,5 @@
+export default function Category({params}){
+    return(
+    <h1>{params.category}</h1>
+    )
+}
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -6,17 +6,18 @@ const Portfolio = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Choose </h1>
+
       <div className={styles.items}>
 
         <Link className={styles.item} href="/portfolio/illustrations">
         <span className={styles.title}>Illustartions</span>
         </Link>
 
-        <Link className={styles.item} href="/portfolio/illustrations">
+        <Link className={styles.item} href="/portfolio/websites">
         <span className={styles.title}>Websites</span>
         </Link>
 
-        <Link className={styles.item} href="/portfolio/illustrations">
+        <Link className={styles.item} href="/portfolio/applications">
         <span className={styles.title}>Applications</span>
         </Link>
 
